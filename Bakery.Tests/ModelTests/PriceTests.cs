c215daf6bbb93eb7aa376ebf9a37e4bcf3c6bcc9
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-15 14:53:41 -0700

    test 7 passing but breaks test 6, check all scenarios for pastries and CreatePastryPrice is functioning as designed, will move onto Program.cs

--- Bakery.Tests/ModelTests/PriceTests.cs
+++ Bakery.Tests/ModelTests/PriceTests.cs
@@ -49,14 +49,23 @@ namespace Bakery.Test
         Bread newBread = new Bread(breadQuantity, breadCost);
         Assert.AreEqual(breadPriceForThree, newBread.CreateBreadPrice());
       }
+      // [TestMethod]
+      // public void PastryPrice_GetPriceOfThreePastries_Int()
+      // {
+      //   int pastryPriceForThree = 5;
+      //   int pastryCost = 2;
+      //   int pastryQuantity = 3;
+      //   Pastry newPastry = new Pastry(pastryCost, pastryQuantity);
+      //   Assert.AreEqual(pastryPriceForThree, newPastry.CreatePastryPrice());
+      // }
       [TestMethod]
-      public void PastryPrice_GetPriceOfThreePastries_Int()
+      public void PastryPrice_GetPriceOfFourPastries_Int()
       {
-        int pastryPriceForThree = 5;
+        int pastryPriceForFour = 10;
         int pastryCost = 2;
-        int pastryQuantity = 1;
+        int pastryQuantity = 6;
         Pastry newPastry = new Pastry(pastryCost, pastryQuantity);
-        Assert.AreEqual(pastryPriceForThree, newPastry.CreatePastryPrice());
+        Assert.AreEqual(pastryPriceForFour, newPastry.CreatePastryPrice());
       }
     }
   }
